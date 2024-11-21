b18366c63e345cb1bd293fbec38b6daafe707cce
Arthi Arumugam <62213284+Art605@users.noreply.github.com>
2024-11-21 07:30:41 +0000

    Update dag.py
    Signed-off-by: Arthi Arumugam <62213284+Art605@users.noreply.github.com>

--- dag.py
+++ dag.py
@@ -4,7 +4,7 @@ from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
 from airflow.providers.google.cloud.operators.datafusion import CloudDataFusionStartPipelineOperator
 
-# Default Arguments for the DAG
+# Default for the DAG
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2023, 12, 18),
@@ -20,14 +20,14 @@ default_args = {
 dag = DAG('employee_data',
           default_args=default_args,
           description='Runs an external Python script and starts Cloud DataFusion pipeline',
-          schedule_interval='@daily',  # Run daily
-          catchup=False)  # Disable catching up missed runs
+          schedule_interval='@daily',  
+          catchup=False) 
 
 with dag:
     # BashOperator to run a Python script (make sure the path is correct)
     run_script_task = BashOperator(
         task_id='extract_data',
-        bash_command='python/opt/airflow/dags/extract.py',  # Adjusted path
+        bash_command='python/opt/airflow/dags/extract.py', 
     )
 
     # Cloud DataFusion task to start a pipeline
@@ -38,5 +38,5 @@ with dag:
         task_id="start_datafusion_pipeline",
     )
 
-    # Setting dependencies: run the extract_data task first, then start the pipeline
+    # Setting dependencies
     run_script_task >> start_pipeline
